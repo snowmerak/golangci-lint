ed2ec93f7cbf2c98ec837c566c667cb0bc995b4f
snowmerak <snowmerak@outlook.com>
2024-06-30 22:32:06 +0900

    refactoring

--- pkg/custom/snowygo/snowygo.go
+++ pkg/custom/snowygo/snowygo.go
@@ -31,36 +31,46 @@ type Pair struct {
 	Second *PairElement
 }
 
+const (
+	LibraryGroup   = "lib"
+	LibraryClient  = "client"
+	LibraryServer  = "server"
+	LibraryUtility = "util"
+
+	InternalGroup = "internal"
+	CommandGroup  = "cmd"
+	ModelGroup    = "model"
+)
+
+func splitPackagePath(path string) (string, []string) {
+	parts := strings.Split(path, "/")
+	if len(parts) < 2 {
+		return "", parts
+	}
+	return parts[0], parts[1:]
+}
+
 func runAnalyzer(cfg *config.SnowyGoSettings) func(pass *analysis.Pass) (interface{}, error) {
 	return func(pass *analysis.Pass) (interface{}, error) {
 		a2b := make(map[string]*Pair)
+		errorStructMap := make(map[string]Reporter)
+		errorCheckerMap := make(map[string]Reporter)
 
 		for _, f := range pass.Files {
 			ast.Inspect(f, func(node ast.Node) bool {
-				// when call expression is found
-				if node, ok := node.(*ast.CallExpr); ok {
-					// when selector expression is found
-					if sel, ok := node.Fun.(*ast.SelectorExpr); ok {
-						// check if fmt.Print* is used
-						if x, ok := sel.X.(*ast.Ident); ok {
-							if x.Name == "fmt" {
-								switch sel.Sel.Name {
-								case "Print":
-									pass.Reportf(node.Pos(), "fmt.Print should not be used")
-								case "Printf":
-									pass.Reportf(node.Pos(), "fmt.Printf should not be used")
-								case "Println":
-									pass.Reportf(node.Pos(), "fmt.Println should not be used")
-								}
-							}
-						}
-					}
-				}
+				// Get the package path
+				packagePath := pass.Pkg.Path()
+				_ = packagePath
 
-				// when variable declaration is found
-				if genDecl, ok := node.(*ast.GenDecl); ok {
-					if genDecl.Tok == token.VAR {
-						for _, spec := range genDecl.Specs {
+				switch node := node.(type) {
+				// when make goroutine is found
+				case *ast.GoStmt:
+					pass.Reportf(node.Pos(), "should not use raw goroutine, use goroutine pool instead")
+				case *ast.GenDecl: // when variable declaration is found
+					// check if global variable is exported
+					switch node.Tok {
+					case token.VAR: // when variable declaration is found
+						for _, spec := range node.Specs {
 							if valueSpec, ok := spec.(*ast.ValueSpec); ok {
 								if len(valueSpec.Names) > 0 {
 									if valueSpec.Names[0].IsExported() {
@@ -69,79 +79,100 @@ func runAnalyzer(cfg *config.SnowyGoSettings) func(pass *analysis.Pass) (interfa
 								}
 							}
 						}
+					case token.TYPE: // when type declaration is found
+						for _, spec := range node.Specs {
+							if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+								if _, ok := typeSpec.Type.(*ast.StructType); ok {
+									if typeSpec.Name.IsExported() && strings.HasSuffix(typeSpec.Name.Name, "Error") {
+										errorStructMap[typeSpec.Name.Name] = Reporter{
+											report: pass.Reportf,
+											pos:    typeSpec.Pos(),
+										}
+									}
+								}
+							}
+						}
 					}
-				}
-
-				// when function declaration is found
-				if fn, ok := node.(*ast.FuncDecl); ok {
-					// check if function name has prefix, 'Request', 'Reply', 'Send', 'Receive', 'Publish', 'Subscribe', 'Set', 'Get'
-					trimmedName := strings.TrimPrefix(fn.Name.Name, "Request")
-					trimmedName = strings.TrimPrefix(trimmedName, "Reply")
-					trimmedName = strings.TrimPrefix(trimmedName, "Send")
-					trimmedName = strings.TrimPrefix(trimmedName, "Receive")
-					trimmedName = strings.TrimPrefix(trimmedName, "Publish")
-					trimmedName = strings.TrimPrefix(trimmedName, "Subscribe")
-					trimmedName = strings.TrimPrefix(trimmedName, "Set")
-					trimmedName = strings.TrimPrefix(trimmedName, "Get")
-					p := a2b[f.Name.Name+"."+trimmedName]
-					if p == nil {
-						p = &Pair{}
-						a2b[f.Name.Name+"."+trimmedName] = p
-					}
-					reporter := Reporter{
-						report: pass.Reportf,
-						pos:    fn.Pos(),
-					}
-
+				case *ast.FuncDecl: // when function declaration is found
 					switch {
-					case strings.HasPrefix(fn.Name.Name, "Request"):
-						p.First = &PairElement{
-							Name:     "Request",
-							Reporter: reporter,
+					case strings.HasPrefix(node.Name.Name, "Is") && strings.HasSuffix(node.Name.Name, "Error"): // when function name has prefix, 'Check'
+						if _, ok := errorCheckerMap[node.Name.Name]; !ok {
+							errorCheckerMap[node.Name.Name] = Reporter{
+								report: pass.Reportf,
+								pos:    node.Pos(),
+							}
 						}
-					case strings.HasPrefix(fn.Name.Name, "Reply"):
-						p.Second = &PairElement{
-							Name:     "Reply",
-							Reporter: reporter,
+					case strings.HasPrefix(node.Name.Name, "New"): // when function name has prefix, 'New'
+					default:
+						// check if function name has prefix, 'Request', 'Reply', 'Send', 'Receive', 'Publish', 'Subscribe', 'Set', 'Get'
+						trimmedName := strings.TrimPrefix(node.Name.Name, "Request")
+						trimmedName = strings.TrimPrefix(trimmedName, "Reply")
+						trimmedName = strings.TrimPrefix(trimmedName, "Send")
+						trimmedName = strings.TrimPrefix(trimmedName, "Receive")
+						trimmedName = strings.TrimPrefix(trimmedName, "Publish")
+						trimmedName = strings.TrimPrefix(trimmedName, "Subscribe")
+						trimmedName = strings.TrimPrefix(trimmedName, "Set")
+						trimmedName = strings.TrimPrefix(trimmedName, "Get")
+						p := a2b[f.Name.Name+"."+trimmedName]
+						if p == nil {
+							p = &Pair{}
+							a2b[f.Name.Name+"."+trimmedName] = p
 						}
-					case strings.HasPrefix(fn.Name.Name, "Send"):
-						p.First = &PairElement{
-							Name:     "Send",
-							Reporter: reporter,
+						reporter := Reporter{
+							report: pass.Reportf,
+							pos:    node.Pos(),
 						}
-					case strings.HasPrefix(fn.Name.Name, "Receive"):
-						p.Second = &PairElement{
-							Name:     "Receive",
-							Reporter: reporter,
-						}
-					case strings.HasPrefix(fn.Name.Name, "Publish"):
-						p.First = &PairElement{
-							Name:     "Publish",
-							Reporter: reporter,
-						}
-					case strings.HasPrefix(fn.Name.Name, "Subscribe"):
-						p.Second = &PairElement{
-							Name:     "Subscribe",
-							Reporter: reporter,
-						}
-					case strings.HasPrefix(fn.Name.Name, "Set"):
-						p.First = &PairElement{
-							Name:     "Set",
-							Reporter: reporter,
-						}
-					case strings.HasPrefix(fn.Name.Name, "Get"):
-						p.Second = &PairElement{
-							Name:     "Get",
-							Reporter: reporter,
+
+						switch {
+						case strings.HasPrefix(node.Name.Name, "Request"):
+							p.First = &PairElement{
+								Name:     "Request",
+								Reporter: reporter,
+							}
+						case strings.HasPrefix(node.Name.Name, "Reply"):
+							p.Second = &PairElement{
+								Name:     "Reply",
+								Reporter: reporter,
+							}
+						case strings.HasPrefix(node.Name.Name, "Send"):
+							p.First = &PairElement{
+								Name:     "Send",
+								Reporter: reporter,
+							}
+						case strings.HasPrefix(node.Name.Name, "Receive"):
+							p.Second = &PairElement{
+								Name:     "Receive",
+								Reporter: reporter,
+							}
+						case strings.HasPrefix(node.Name.Name, "Publish"):
+							p.First = &PairElement{
+								Name:     "Publish",
+								Reporter: reporter,
+							}
+						case strings.HasPrefix(node.Name.Name, "Subscribe"):
+							p.Second = &PairElement{
+								Name:     "Subscribe",
+								Reporter: reporter,
+							}
+						case strings.HasPrefix(node.Name.Name, "Set"):
+							p.First = &PairElement{
+								Name:     "Set",
+								Reporter: reporter,
+							}
+						case strings.HasPrefix(node.Name.Name, "Get"):
+							p.Second = &PairElement{
+								Name:     "Get",
+								Reporter: reporter,
+							}
 						}
 					}
 
 					// when function has parameters
-					if fn.Type.Params != nil {
+					if node.Type.Params != nil {
 						// check if the function has a context.Context parameter
 						hasCtx := false
 						isCtxFirst := false
-						for i, param := range fn.Type.Params.List {
+						for i, param := range node.Type.Params.List {
 							if len(param.Names) >= 1 && param.Names[0].Name == "ctx" {
 								hasCtx = true
 								if i == 0 {
@@ -153,18 +184,18 @@ func runAnalyzer(cfg *config.SnowyGoSettings) func(pass *analysis.Pass) (interfa
 
 						// report if the function has a context.Context parameter but it's not the first parameter
 						if hasCtx && !isCtxFirst {
-							pass.Reportf(fn.Pos(), "context.Context should be the first parameter")
+							pass.Reportf(node.Pos(), "context.Context should be the first parameter")
 						}
 					}
 
 					// when function has return values
-					if fn.Type.Results != nil {
+					if node.Type.Results != nil {
 						hasErr := false
 						isErrLast := false
-						for i, param := range fn.Type.Results.List {
+						for i, param := range node.Type.Results.List {
 							if ident, ok := param.Type.(*ast.Ident); ok && ident.Name == "error" {
 								hasErr = true
-								if i == len(fn.Type.Results.List)-1 {
+								if i == len(node.Type.Results.List)-1 {
 									isErrLast = true
 								}
 							}
@@ -172,30 +203,27 @@ func runAnalyzer(cfg *config.SnowyGoSettings) func(pass *analysis.Pass) (interfa
 
 						// report if the function has an error return value but it's not the last return value
 						if hasErr && !isErrLast {
-							pass.Reportf(fn.Pos(), "error should be the last return value")
+							pass.Reportf(node.Pos(), "error should be the last return value")
 						}
 					}
 
 					// when function has a body
-					if len(fn.Body.List) != 0 {
-						for _, stmt := range fn.Body.List {
+					if len(node.Body.List) != 0 {
+						for _, stmt := range node.Body.List {
 							// check if if statement has else branch
 							if stmt, ok := stmt.(*ast.IfStmt); ok {
 								if stmt.Else != nil {
 									pass.Reportf(stmt.Pos(), "if statement should not have an else branch, use early return or switch statement instead")
 								}
 							}
-
-							// when return statement is found
-							if stmt, ok := stmt.(*ast.ReturnStmt); ok {
-								// check if return statement has error and no wrapping
-								if len(stmt.Results) > 0 {
-									for _, result := range stmt.Results[1:] {
-										if ident, ok := result.(*ast.Ident); ok && ident.Name == "err" {
-											pass.Reportf(stmt.Pos(), "error should not be wrapped")
-										}
-									}
-								}
+						}
+					}
+				case *ast.ReturnStmt: // when return statement is found
+					// check if return statement has error and no wrapping
+					if len(node.Results) > 0 {
+						for _, result := range node.Results {
+							if ident, ok := result.(*ast.Ident); ok && ident.Name == "err" {
+								pass.Reportf(node.Pos(), "error should not be wrapped")
 							}
 						}
 					}
@@ -203,6 +231,20 @@ func runAnalyzer(cfg *config.SnowyGoSettings) func(pass *analysis.Pass) (interfa
 
 				return true
 			})
+		}
+
+		for e := range errorStructMap {
+			c := "Is" + e
+			if _, ok := errorCheckerMap[c]; !ok {
+				errorStructMap[e].report(errorStructMap[e].pos, "missing %s function", c)
+			}
+			delete(errorCheckerMap, c)
+		}
+		for c := range errorCheckerMap {
+			e := strings.TrimPrefix(c, "Is")
+			if _, ok := errorStructMap[e]; !ok {
+				errorCheckerMap[c].report(errorCheckerMap[c].pos, "missing %s struct", e)
+			}
 		}
 
 		for _, pair := range a2b {
