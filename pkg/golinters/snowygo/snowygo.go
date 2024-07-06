a12319405a6f382f344476f0403640765038be99
snowmerak <snowmerak@outlook.com>
2024-07-06 17:56:06 +0900

    extract snowygo

--- pkg/golinters/snowygo/snowygo.go
+++ pkg/golinters/snowygo/snowygo.go
@@ -2,13 +2,13 @@ package snowygo
 
 import (
 	"github.com/snowmerak/golangci-lint/pkg/config"
-	"github.com/snowmerak/golangci-lint/pkg/custom/snowygo"
 	"github.com/snowmerak/golangci-lint/pkg/goanalysis"
+	"github.com/snowmerak/snowygo"
 	"golang.org/x/tools/go/analysis"
 )
 
 func New(settings *config.SnowyGoSettings) *goanalysis.Linter {
-	a := snowygo.NewAnalyzerWithConfig(settings)
+	a := snowygo.NewAnalyzerWithConfig(&snowygo.Config{})
 
 	return goanalysis.
 		NewLinter(a.Name, a.Doc, []*analysis.Analyzer{a}, nil).
